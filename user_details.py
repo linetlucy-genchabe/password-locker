1450cacca35d660a1823b820e76e5a4f4e709dab
linetlucy-genchabe <linetlucy21@gmail.com>
2022-04-23 14:17:28 +0300

    add class to create user accounts

--- user_details.py
+++ user_details.py
@@ -3,3 +3,26 @@ import random
 import string
 
 global users_list
+
+class User:
+	'''
+	Class to create user accounts and save their information
+	'''
+	# Class Variables
+	# global users_list
+	users_list = []
+	def __init__(self,first_name,last_name,password):
+		'''
+		Method to define the properties for each user object will hold.
+		'''
+
+		# instance variables
+		self.first_name = first_name
+		self.last_name = last_name
+		self.password = password
+
+	def save_user(self):
+		'''
+		Function to save a newly created user instance
+		'''
+		User.users_list.append(self)
