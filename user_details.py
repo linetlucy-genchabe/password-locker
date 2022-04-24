09858f42fe27e8c599997c9bc88ac7d950ec3c9f
linetlucy-genchabe <linetlucy21@gmail.com>
2022-04-24 10:34:07 +0300

    add class to create account credentials

--- user_details.py
+++ user_details.py
@@ -2,21 +2,20 @@ import pyperclip
 import random
 import string
 
-global users_list
+
 
 class User:
 	'''
 	Class to create user accounts and save their information
 	'''
-	# Class Variables
-	# global users_list
+
 	users_list = []
 	def __init__(self,first_name,last_name,password):
 		'''
 		Method to define the properties for each user object will hold.
 		'''
 
-		# instance variables
+        
 		self.first_name = first_name
 		self.last_name = last_name
 		self.password = password
@@ -26,3 +25,12 @@ class User:
 		Function to save a newly created user instance
 		'''
 		User.users_list.append(self)
+
+		
+class Credential:
+	'''
+	Class to create  account credentials, generate passwords and save their information
+	'''
+	
+	credentials_list =[]
+	user_credentials_list = []
