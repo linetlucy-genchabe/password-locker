2438b0a7008786ea8991b7a2c7332fbac1a474d7
linetlucy-genchabe <linetlucy21@gmail.com>
2022-04-24 11:53:35 +0300

    add functions to generate,display and save user credentials

---
+++ password_locker.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+import pyperclip
+from user_details import User, Credential
+
+def create_user(fname,lname,password):
+	'''
+	Function to create a new user account
+	'''
+	new_user = User(fname,lname,password)
+	return new_user
+
+def save_user(user):
+	'''
+	Function to save a new user account
+	'''
+	User.save_user(user)
+
+
+def verify_user(first_name,password):
+	'''
+	Function that verifies the existance of the user before creating credentials
+	'''
+	checking_user = Credential.check_user(first_name,password)
+	return checking_user
+
+def generate_password():
+	'''
+	Function to generate a password automatically
+	'''
+	gen_pass = Credential.generate_password()
+	return gen_pass
+
+def create_credential(user_name,site_name,account_name,password):
+	'''
+	Function to create a new credential
+	'''
+	new_credential=Credential(user_name,site_name,account_name,password)
+	return new_credential
+
+def save_credential(credential):
+	'''
+	Function to save a newly created credential
+	'''
+	Credential.save_credentials(credential)
+
+def display_credentials(user_name):
+	'''
+	Function to display credentials saved by a user
+	'''
+	return Credential.display_credentials(user_name)
+	
+def copy_credential(site_name):
+	'''
+	Function to copy a credentials details to the clipboard
+	'''
+	return Credential.copy_credential(site_name)
