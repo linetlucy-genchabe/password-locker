e45544ac4b8331bffaf26fb0b05265346a37f6f5
linetlucy-genchabe <linetlucy21@gmail.com>
2022-04-24 10:36:57 +0300

    add method that defines properties for each user

--- user_details.py
+++ user_details.py
@@ -44,3 +44,14 @@ class Credential:
 			if (user.first_name == first_name and user.password == password):
 				current_user = user.first_name
 		return current_user
+
+    def __init__(self,user_name,site_name,account_name,password):
+		'''
+		Method to define the properties for each user object will hold.
+		'''
+
+		# instance variables
+		self.user_name = user_name
+		self.site_name = site_name
+		self.account_name = account_name
+		self.password = 
