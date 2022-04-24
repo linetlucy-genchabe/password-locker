6c7c4db6e64048bfe23cf699fcc1661016fe6f13
linetlucy-genchabe <linetlucy21@gmail.com>
2022-04-24 11:06:36 +0300

    add credentials test class

--- details_test.py
+++ details_test.py
@@ -13,14 +13,14 @@ class TestUser(unittest.TestCase):
 		'''
 		Function to create a user account before each test
 		'''
-		self.new_user = User('Mary','Ng\'ang\'a','pswd100')
+		self.new_user = User('lynne','genchabe','pswd100')
 
 	def test__init__(self):
 		'''
 		Test to if check the initialization/creation of user instances is properly done
 		'''
-		self.assertEqual(self.new_user.first_name,'Mary')
-		self.assertEqual(self.new_user.last_name,'Ng\'ang\'a')
+		self.assertEqual(self.new_user.first_name,'lynne')
+		self.assertEqual(self.new_user.last_name,'genchabe')
 		self.assertEqual(self.new_user.password,'pswd100')
 
 	def test_save_user(self):
@@ -29,3 +29,25 @@ class TestUser(unittest.TestCase):
 		'''
 		self.new_user.save_user()
 		self.assertEqual(len(User.users_list),1)
+
+class TestCredentials(unittest.TestCase):
+	'''
+	Test class that defines test cases for the credentials class behaviours.
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+	def test_check_user(self):
+		'''
+		Function to test whether the login in function check_user works as expected
+		'''
+		self.new_user = User('lynne','genchabe','pswd100')
+		self.new_user.save_user()
+		user2 = User('linet','lucy','pswd100')
+		user2.save_user()
+
+		for user in User.users_list:
+			if user.first_name == user2.first_name and user.password == user2.password:
+				current_user = user.first_name
+		return current_user
+
+		self.assertEqual(current_user,Credential.check_user(user2.password,user2.first_name))
